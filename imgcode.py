2e13997b65d27987858dbd2a439cac1275e545ea
Parent Jerome <jejmule@gmail.com>
2020-01-08 20:11:02 +0100

    unniform filter added

--- imgcode.py
+++ imgcode.py
@@ -1,33 +1,33 @@
 # imgcode v0.3 08/04/2019
 # utility for CNC lasers image etching
-# developed by M. "Vidmo" Widomski  
+# developed by M. "Vidmo" Widomski
 # github.com/vidmo91
 # hackaday.io/vidmo91
-# 
+#
 # correct execution command: python imgcode.py image_path output_file_path x_offset_mm y_offset_mm output_image_horizontal_size_mm pixel_size_mm feedrate max_laser_power number_of_colours
 # e.g. of correct execution commands:
 # python .\imgcode.py "C:\lena.png" test.nc 0 0 10 0.5 100 1000 2
 # python .\imgcode.py lena.png test.nc 0 0 10 0.2 220 255 5
-# 
+#
 # requirements contains list of modules I had it working on
-# 
+#
 # todo:
-# 
+#
 # check and correct variable types, round floats
 # add some GUI maybe?
 #
-# 
-# 
-# 
+#
+#
+#
 
 # Copyright 2019 M.Widomski
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -37,6 +37,7 @@
 import numpy
 import matplotlib
 import matplotlib.pyplot
+import scipy.ndimage as ndimage
 import imageio
 import PIL.Image
 import sys
@@ -77,7 +78,7 @@ if len(sys.argv) != 10:
     print("correct execution command: ")
     print("python imgcode.py image_path output_file_path x_offset_mm y_offset_mm output_image_horizontal_size_mm pixel_size_mm feedrate max_laser_power number_of_colours")
     print("e.g. python .\\imgcode.py lena.png test.nc 0 0 10 0.2 100 255 5")
-    print("e.g. python .\\imgcode.py \"C:\\Documents\\laser files\\lena.png\" \"C:\\laser files\\out files\\output_gcode.nc\" 0 0 10 0.2 100 255 5"+colorama.Fore.RESET)    
+    print("e.g. python .\\imgcode.py \"C:\\Documents\\laser files\\lena.png\" \"C:\\laser files\\out files\\output_gcode.nc\" 0 0 10 0.2 100 255 5"+colorama.Fore.RESET)
     raise NameError("wrong execution command")
 print("so far so good, now parsing values...")
 
@@ -110,7 +111,7 @@ print("processing...")
 y_size_input = len(img)
 x_size_input = len(img[0])
 
-# scale calculation 
+# scale calculation
 x_size_output = output_image_horizontal_size_mm/pixel_size_mm
 scale = x_size_output/x_size_input
 # reseize image
@@ -122,12 +123,15 @@ img = numpy.asarray(img)
 y_size_output = len(img)
 x_size_output = len(img[0])
 
-# negative for laser etching 
+# negative for laser etching
 img=numpy.subtract(255,img)
 
-# set max value of image colour to number of colours 
+# set max value of image colour to number of colours
 number_of_colours -= 1
 img = numpy.rint(numpy.multiply(img, number_of_colours/255))
+
+# apply uniform filter to smooth power over etchs
+img = ndimage.filters.uniform_filter1d(img,10,axis=1)
 
 #save preview
 img_out=numpy.empty((x_size_output,y_size_output))
@@ -138,17 +142,17 @@ imageio.imwrite('out_img.png',img_out)
 #convert to feedrates
 img = numpy.rint(numpy.multiply(img, max_laser_power/number_of_colours))
 
-# display preview before processing - requires closing plot window before proceeding 
+# display preview before processing - requires closing plot window before proceeding
 # img2=numpy.subtract(number_of_colours,img)
 # matplotlib.pyplot.imshow(img2, cmap='gray')
 # matplotlib.pyplot.show()
 
-# flip up-down for simplicity 
+# flip up-down for simplicity
 img=numpy.flip(img,0)
 
 #Gcode processing
 f.write("( imgcode generated code )\n")
-f.write("( developed by M. \"Vidmo\" Widomski )\n") 
+f.write("( developed by M. \"Vidmo\" Widomski )\n")
 f.write("(  github.com/vidmo91 )\n")
 f.write("(  hackaday.io/vidmo91 )\n")
 f.write(" \n")
@@ -159,55 +163,55 @@ f.write(" \n") #add your G-CODE file header here
 # f.write("M5 S0\n")
 for y in range(y_size_output):
     prev_power=int(0)
-  
+
     if 1-y%2:
         # prev_power=int(0)
         for x in range(x_size_output):
             if (x == 0  and img[y][x] != 0): #first point, diffrent from 0
-                f.write("G0 X"+str(round(x*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")                                                                                                              
-                f.write("M3 S"+str(int(img[y][x]))+"\n")                                                                     
+                f.write("G0 X"+str(round(x*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
+                f.write("M3 S"+str(int(img[y][x]))+"\n")
                 prev_power = int(img[y][x])
             elif x==(x_size_output-1):#eol
                 if (prev_power==0):
                     f.write("M5 S0\n")
                 else:
-                    f.write("G1 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")      
+                    f.write("G1 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
                     f.write("M5 S0\n")
                 prev_power=0
             elif (prev_power != img[y][x]):#different power
                 if (prev_power==0): #transition from 0 to higher power
-                    f.write("G0 X"+str(round((x-1)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")      
-                    f.write("M3 S"+str(int(img[y][x]))+"\n") 
+                    f.write("G0 X"+str(round((x-1)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
+                    f.write("M3 S"+str(int(img[y][x]))+"\n")
                     prev_power = int(img[y][x])
                 if(prev_power != 0):# transition from some power to another
-                    f.write("G1 X"+str(round((x-1)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")      
-                    f.write("M3 S"+str(int(img[y][x]))+"\n")  
+                    f.write("G1 X"+str(round((x-1)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
+                    f.write("M3 S"+str(int(img[y][x]))+"\n")
                     prev_power = int(img[y][x])
     else:
         # prev_power=int(0)
         for x in reversed(range(x_size_output)):
             if (x == x_size_output-1  and img[y][x] != 0): #first point, diffrent from 0
-                f.write("G0 X"+str(round(x*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")                                                                                                              
-                f.write("M3 S"+str(int(img[y][x]))+"\n")                                                                     
+                f.write("G0 X"+str(round(x*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
+                f.write("M3 S"+str(int(img[y][x]))+"\n")
                 prev_power = int(img[y][x])
             elif x==0:#eol
                 if (prev_power==0):
                     f.write("M5 S0\n")
                 else:
-                    f.write("G1 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")      
+                    f.write("G1 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
                     f.write("M5 S0\n")
                 prev_power=0
             elif (prev_power != img[y][x]):#different power
                 if (prev_power==0): #transition from 0 to higher power
-                    f.write("G0 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")      
-                    f.write("M3 S"+str(int(img[y][x]))+"\n")                                                                     
+                    f.write("G0 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
+                    f.write("M3 S"+str(int(img[y][x]))+"\n")
                     prev_power = int(img[y][x])
                 if(prev_power != 0):# transition from some power to another
-                    f.write("G1 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")      
-                    f.write("M3 S"+str(int(img[y][x]))+"\n")                                                                     
+                    f.write("G1 X"+str(round((x)*pixel_size_mm+x_offset_mm,4))+" Y" + str(round(y*pixel_size_mm+y_offset_mm,4))+"\n")
+                    f.write("M3 S"+str(int(img[y][x]))+"\n")
                     prev_power = int(img[y][x])
 f.close()
-            
+
 #input("everything done, press ENTER to exit, goodbye!")
 print(colorama.Fore.GREEN+"\neverything done, buh bye!\n")
 input("press ENTER to exit")
