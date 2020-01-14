4cec40f7820d05315d22b0dfa464d504ecd415d8
Parent Jerome <jerome.parent@lynceetec.com>
2020-01-14 12:23:21 +0100

    updated output

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ Created on Mon Jan 13 12:56:18 2020
 @author: jparent
 """
 
-runfile('D:/jparent/Documents/GitHub/imgcode/imgcode.py', wdir='D:/jparent/Documents/GitHub/imgcode', args='limpid.png test.nc 0 0 50 0.25 3000 255 2')
+runfile('D:/jparent/Documents/GitHub/imgcode/imgcode.py', wdir='D:/jparent/Documents/GitHub/imgcode', args='limpid.png test.nc 0 0 50 0.25 1000 255 2')
